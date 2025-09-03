d6c8df5f0076a79810ecb915ac130309a8eed3b1
BART! <bart@bartweb.cz>
2025-09-03 17:33:17 +0200

    chore(deps): eslint migration

---
+++ eslint.config.mjs
@@ -0,0 +1,52 @@
+import { defineConfig } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("google", "plugin:prettier/recommended"),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+        prettier,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+            Atomics: "readonly",
+            SharedArrayBuffer: "readonly",
+        },
+
+        parser: tsParser,
+        ecmaVersion: 2024,
+        sourceType: "module",
+    },
+
+    rules: {
+        "require-jsdoc": "off",
+        "no-unused-vars": "off",
+
+        "@typescript-eslint/no-unused-vars": ["error", {
+            vars: "all",
+            args: "after-used",
+            ignoreRestSiblings: false,
+        }],
+
+        "prettier/prettier": "error",
+    },
+}]);
